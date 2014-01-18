ee07ccb976aaeb6648ba25520bc8b6432129c82d
stonie <stonie@Drivenlogic-One>
2014-01-18 13:58:01 +1100

    some refactoring toward cleaner patterns

---
+++ HolidayProject/Holiday/src/main/java/au/com/risingedge/holiday/Helpers.java
@@ -0,0 +1,40 @@
+package au.com.risingedge.holiday;
+
+import android.util.Log;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+///
+/// Class for helper methods that don't fit with types.
+///
+public class Helpers {
+
+    private static final String TAG = "holidayHelpers";
+
+    ///
+    /// Get local IP Address.
+    //
+    public static InetAddress getLocalInetAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress;
+                    }
+
+                    // TODO: Future - add ipV6 support.
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+            Log.e(TAG, "Error when looking up IP address", ex);
+        }
+        return null;
+    }
+}
--- HolidayProject/Holiday/src/main/java/au/com/risingedge/holiday/MainActivity.java
+++ HolidayProject/Holiday/src/main/java/au/com/risingedge/holiday/MainActivity.java
@@ -1,114 +1,151 @@
 package au.com.risingedge.holiday;
 
 import javax.jmdns.JmDNS;
-import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceListener;
 import javax.jmdns.ServiceInfo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.text.format.Formatter;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import java.io.IOException;
-import java.math.BigInteger;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.nio.ByteOrder;
-import java.util.Enumeration;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "holidayMainActivity";
 
-    android.net.wifi.WifiManager.MulticastLock lock;
-    android.os.Handler handler = new android.os.Handler();
-
-    LinearLayout horizontalLinearLayout;
-
-    private Context activityContext;
-
-    //private String mdnsType = "_iotas._tcp.local."; // _service._protocol.local.
-    private String mdnsType = "_sleep-proxy._udp.local.";
-    private JmDNS jmdns = null;
-    private ServiceListener listener = null;
-    private ServiceInfo serviceInfo;
+    WifiManager.MulticastLock _multicastLock;
+    private Handler _mdnsListenerhandler = new Handler();
+    private Handler _uiHandler;
+    private String _mdnsServiceType = "_iotas._tcp.local."; // _service._protocol.local. // TODO: move to settings.
+    private JmDNS _jmdns = null;
+    private Context _context;
+    private ServiceListener _listener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         // catch unhandled Exceptions
         Thread.setDefaultUncaughtExceptionHandler(new DefaultExceptionHandler());
 
-        activityContext = this;
-
-        // full screen
+        // go full screen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        _uiHandler = new Handler(); // UI thread
+        _context = this; // REVIEW: is there a better way to do this? I need to pass the context to a button constructor.
+
+        // Get WiFi manager and enable multicast
+        android.net.wifi.WifiManager wifiManager = (android.net.wifi.WifiManager)getSystemService(android.content.Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled()){
+            Log.w(TAG,"WiFi is off! - Can't scan - please enable WiFi");
+
+            // TODO: show user a dialog
+        }
+        else
+        {
+            // Take multicast lock
+            _multicastLock = wifiManager.createMulticastLock("multiCastLock");
+            _multicastLock.setReferenceCounted(true);
+            _multicastLock.acquire();
+
+            // Start scan
+            StartScan(this);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        handler.postDelayed(new Runnable() {
+    ///
+    /// Start the mdns scan
+    ///
+    void StartScan(final MainActivity mainActivity) // REVIEW: is there a better pattern than passing a final activity to a runnable?
+    {
+        _mdnsListenerhandler.postDelayed(
+        new Runnable() {
             public void run() {
                 try
                 {
-                    android.net.wifi.WifiManager wifiManager = (android.net.wifi.WifiManager)getSystemService(android.content.Context.WIFI_SERVICE);
-                    lock = wifiManager.createMulticastLock("multiCastLock");
-                    lock.setReferenceCounted(true);
-                    lock.acquire();
-
-                    Logger logger = Logger.getLogger(JmDNS.class.getName());
-                    ConsoleHandler handler = new ConsoleHandler();
-                    logger.addHandler(handler);
-                    logger.setLevel(Level.FINER);
-                    handler.setLevel(Level.FINER);
-
-                    String ipAddress = getLocalInetAddress().getHostAddress();
-
+                    String ipAddress = Helpers.getLocalInetAddress().getHostAddress();
                     Log.i(TAG,"Local IP Address is: " + ipAddress);
 
-                    final JmDNS jmdns = JmDNS.create(getLocalInetAddress(),ipAddress);
-                    jmdns.addServiceListener("_iotas._tcp.local.", new SampleListener());
+                    _jmdns = JmDNS.create(Helpers.getLocalInetAddress(),ipAddress);
+                    _jmdns.addServiceListener(_mdnsServiceType, new MdnsListener(_jmdns,mainActivity));
 
-                    Log.i(TAG,"jmDNS configured.");
-
-                    ServiceInfo[] serviceInfos = jmdns.list("_iotas._tcp.local.");
-
-                    for(int i =0; i < serviceInfos.length; i++)
-                    {
-                        Log.i(TAG,serviceInfos[i].getQualifiedName());
-                    }
+                    Log.i(TAG,"jmDNS configured...");
                 }
                 catch (IOException e) {
                     e.printStackTrace();
                     Log.e(TAG,"Error on jmDNS setup",e);
                     return;
                 }
+            }},500);
+    }
+
+    ///
+    /// Called for each holiday found - adds a launcher to the menu
+    ///
+    void notifyUser(final String msg, final String url) {
+        _uiHandler.post(new Runnable() {
+            public void run() {
+
+                Log.i(TAG, "Creating button - " + msg);
+                LinearLayout linearLayout = (LinearLayout) findViewById(R.id.verticalLinearLayout);
+                //  LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                // button for each holiday located
+                Button button = new Button(_context);
+                button.setOnClickListener(new HolidayClickListener(url));
+                button.setText(msg);
+
+                //  horizontalLinearLayout.addView(button, layoutParams);
+                linearLayout.addView(button);
             }
-        }, 1000);
+        });
+    }
 
-        //Button myButton = new Button(this);
-        //myButton.setText("Push Me");
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //new Thread(){public void run() {setUp();}}.start();
+    }
 
-        //horizontalLinearLayout = (LinearLayout)findViewById(R.id.horizontalLinearLayout);
-        //LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        //horizontalLinearLayout.addView(myButton, lp);
+    ///
+    /// Clean up on stop - multicast chews battery.
+    ///
+    @Override
+    protected void onStop() {
+        _multicastLock.release(); // release multicast lock.
+        if (_jmdns != null) {
+            if (_listener != null) {
+                _jmdns.removeServiceListener(_mdnsServiceType, _listener);
+                _listener = null;
+            }
+            _jmdns.unregisterAllServices();
+            try {
+                _jmdns.close();
+            } catch (IOException e) {
 
+                e.printStackTrace();
+                Log.e(TAG,"Error on stopping",e);
+            }
+            _jmdns = null;
+        }
+
+        super.onStop();
     }
 
     @Override
@@ -131,163 +168,22 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setUp() {
-
-        android.net.wifi.WifiManager wifiManager = (android.net.wifi.WifiManager)getSystemService(android.content.Context.WIFI_SERVICE);
-        lock = wifiManager.createMulticastLock("mylockthereturn");
-        lock.setReferenceCounted(true);
-        lock.acquire();
-
-        try {
-
-            jmdns = JmDNS.create(wifiIpAddress(activityContext));
-            jmdns.addServiceListener(mdnsType, listener = new ServiceListener() {
-
-                @Override
-                public void serviceResolved(ServiceEvent ev) {
-                    String additions = "";
-                    if (ev.getInfo().getInetAddresses() != null && ev.getInfo().getInetAddresses().length > 0) {
-                        additions = ev.getInfo().getInetAddresses()[0].getHostAddress();
-                    }
-                    Log.w(TAG, "Service resolved:" + ev.getInfo().getQualifiedName() + " port:" + ev.getInfo().getPort() + additions);
-                    notifyUser("Service resolved: " + ev.getInfo().getQualifiedName() + " port:" + ev.getInfo().getPort() + additions);
-                }
-
-                @Override
-                public void serviceRemoved(ServiceEvent ev) {
-                    notifyUser("Service removed: " + ev.getName());
-                }
-
-                @Override
-                public void serviceAdded(ServiceEvent event) {
-                    // Required to force serviceResolved to be called again (after the first search)
-                    jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
-                }
-            });
-
-        } catch (IOException e) {
-            Log.e(TAG,"Error setring up jmDNS",e);
-            e.printStackTrace();
-            return;
-        }
-    }
-
-
-    private void notifyUser(final String msg) {
-        handler.postDelayed(new Runnable() {
-            public void run() {
-
-                Button myButton = new Button(activityContext);
-                myButton.setText(msg);
-
-                horizontalLinearLayout = (LinearLayout)findViewById(R.id.horizontalLinearLayout);
-
-                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-                horizontalLinearLayout.addView(myButton, lp);
-            }
-        }, 1);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //new Thread(){public void run() {setUp();}}.start();
-    }
-
-    @Override
-    protected void onStop() {
-        if (jmdns != null) {
-            if (listener != null) {
-                jmdns.removeServiceListener(mdnsType, listener);
-                listener = null;
-            }
-            jmdns.unregisterAllServices();
-            try {
-                jmdns.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                Log.e(TAG,"Error on stopping",e);
-            }
-            jmdns = null;
-        }
-        //repo.stop();
-        //s.stop();
-        lock.release();
-        super.onStop();
-    }
-
     ///
-    /// Get local IP Address.
-    //
-    public static InetAddress getLocalInetAddress() {
-        try {
-            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
-                NetworkInterface intf = en.nextElement();
-                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
-                    InetAddress inetAddress = enumIpAddr.nextElement();
-                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
-                        return inetAddress;
-                    }
-
-                    // TODO: Future - add ipV6 support.
-                }
-            }
-        } catch (SocketException ex) {
-            ex.printStackTrace();
-            Log.e(TAG,"Error when looking up IP address",ex);
-        }
-        return null;
-    }
-
-    protected String wifiIpAddress(Context context) {
-        WifiManager wifiManager = (WifiManager) context.getSystemService(WIFI_SERVICE);
-        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
-
-        // Convert little-endian to big-endianif needed
-        if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
-            ipAddress = Integer.reverseBytes(ipAddress);
-        }
-
-        byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
-
-        String ipAddressString;
-        try {
-            ipAddressString = InetAddress.getByAddress(ipByteArray).getHostAddress();
-        } catch (UnknownHostException ex) {
-            Log.w(TAG, "Unable to get host address.");
-            ipAddressString = null;
-        }
-
-        return ipAddressString;
-    }
-
+    /// A click handler class that passes the URL of the given Holiday
     ///
-    /// Implemention of jmDNS listner class.
-    ///
-    class SampleListener implements ServiceListener {
-        @Override
-        public void serviceAdded(ServiceEvent event) {
-            Log.i(TAG,"Service added   : " + event.getName() + "." + event.getType());
-            jmdns.requestServiceInfo(event.getType(), event.getName(), 1);
+    public class HolidayClickListener implements View.OnClickListener
+    {
+        String url;
+        public HolidayClickListener(String url) {
+            this.url = url;
         }
 
         @Override
-        public void serviceRemoved(ServiceEvent event) {
-            Log.i(TAG, "Service removed : " + event.getName() + "." + event.getType());
-        }
-
-        @Override
-        public void serviceResolved(ServiceEvent event) {
-            Log.i(TAG, "Service resolved: " + event.getInfo());
-
-            String additions = "";
-            if (event.getInfo().getInetAddresses() != null && event.getInfo().getInetAddresses().length > 0) {
-                additions = event.getInfo().getInetAddresses()[0].getHostAddress();
-            }
-
-            notifyUser("Service resolved: " + event.getInfo().getQualifiedName() + " port:" + event.getInfo().getPort() + additions);
+        public void onClick(View v)
+        {
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
         }
     }
 }
